643abb282dab650a38b32cc7a67cd3d694ce8734
naufal afif <naufalafif58@gmail.com>
2019-01-17 17:47:21 +0700

    add buefy css

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from  './routers'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 
-Vue.config.productionTip = false
+Vue.use(Buefy)
+
+// Vue.config.productionTip = false
+
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
