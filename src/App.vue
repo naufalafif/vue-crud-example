b3f95bb3fdfa03f71095fba611f03df88cefe120
naufal afif <naufalafif58@gmail.com>
2019-01-17 17:49:04 +0700

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h3>Aplikasi CRUD Menggunakan Vue</h3>
+  <div id="app" class="container">
+
+    <Navbar :links="navigationLink" ></Navbar>
+    <div class="container">    
+      <router-view class="view"></router-view>
+    </div>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'app',
+  components:{
+    Navbar,
+  },
+  created(){
+  },
+  computed:{
+    navigationLink(){
+      return this.$router.options.routes.map(i => {
+          return {
+              title:i.name,
+              url:i.path,
+              child:i.child? i.child : []
+            }
+        });
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
